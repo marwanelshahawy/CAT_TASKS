4d817a02ece3e5a1426024cfa688c16cd42c7211
Marwan Elshahawy <marawanwaleed27@gmail.com>
2024-01-31 02:17:11 +0200

    task 2 finished

---
+++ WEEK1/task2/task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("please enter the number : ");
+    scanf("%d",&x);
+        printf("================ \nThe number is %d",x);
+
+    return 0;
+
+}
